3a1258bd3acde542d3a696ae062ab981f3454f21
Yar177 <yarsher.dev@gmail.com>
2023-01-01 20:07:56 -0500

    checkForDraw and Win

--- TicTacToe/Views/GameScreen/GameViewModel.swift
+++ TicTacToe/Views/GameScreen/GameViewModel.swift
@@ -15,8 +15,9 @@ class GameViewModel: ObservableObject{
     
    @Published var game = Game(id: UUID().uuidString, player1: "player1", player2: "player2", blockMoveForPlayerId: "player2", winnerId: "", rematchPlayerId: [], moves: Array(repeating: nil, count: 9))
     
+    private let winPatterns: Set<Set<Int>> = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+    
     func processPlayerMove(for position: Int){
-        
         if isPositionAvaliable(in: game.moves, forIndex: position){return}
         
         game.moves[position] = Move(isPlayer1: true, boardIndex: position)
@@ -29,5 +30,15 @@ class GameViewModel: ObservableObject{
     func isPositionAvaliable(in moves: [Move?], forIndex index: Int) -> Bool{
         return moves.contains(where: {$0?.boardIndex == index})
     }
+    
+    func checkForWin() -> Bool{
+        
+        return true
+    }
+    
+    func checkForDraw(in moves: [Move?]) -> Bool{
+        return moves.compactMap {$0}.count == 9
+    }
+    
     
 }
