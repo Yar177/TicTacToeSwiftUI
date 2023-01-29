42b93ddeab688554e077c219d357ddda2cb9c54c
Yar177 <yarsher.dev@gmail.com>
2023-01-29 09:41:55 -0500

    startGame

--- TicTacToe/FirebaseUtils/FirebaseService.swift
+++ TicTacToe/FirebaseUtils/FirebaseService.swift
@@ -22,8 +22,23 @@ class FirebaseService: ObservableObject{
     
     func startGame(with userId:String){
         //TODO: check if there is a game to join if not create a game
-        firebaseRefrence(.Game).whereField("player2", isEqualTo: "").whereField("player1", isNotEqualTo: userId).getDocuments { FIRQuerySnapshot, error in
-            <#code#>
+        firebaseRefrence(.Game).whereField("player2", isEqualTo: "").whereField("player1", isNotEqualTo: userId).getDocuments { querySnapshot, error in
+            if error != nil {
+                print("Error starting game ", error?.localizedDescription)
+                //create new game
+                self.createNewgame(with: userId)
+                return
+            }
+            if let gameData = querySnapshot?.documents.first{
+                self.game = try? gameData.data(as: Game.self)
+                self.game.player2 = userId
+                self.game.blockMoveForPlayerId = userId
+                
+                self.updateGame(self.game)
+                self.listeningToGameChanges()
+            }else{
+                self.createNewgame(with: userId)
+            }
         }
     }
     
@@ -32,6 +47,10 @@ class FirebaseService: ObservableObject{
     }
     
     func createNewgame(with userId: String){
+        
+    }
+    
+    func updateGame(_ game: Game){
         
     }
     
